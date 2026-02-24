d5813ed46e25c38484e299ba28aced683a3da9c2
Luisa Rosette <ferosete20@gmail.com>
2026-02-24 15:27:48 -0500

    Proyecto inicial con conexion a MySQL en Railway

---
+++ conexion.py
@@ -0,0 +1,20 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+# Cargar variables del archivo .env
+load_dotenv()
+
+db = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    database=os.getenv("DB_NAME")
+)
+
+cursor = db.cursor()
+cursor.execute("SHOW TABLES")
+
+print("Conectado a Railway")
+print(cursor.fetchall())
